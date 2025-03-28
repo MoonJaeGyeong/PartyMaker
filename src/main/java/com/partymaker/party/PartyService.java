0a65b3d1b4a1c14d5f57bbdee2362a4cd24f6536
MoonJaeGyeong <bblbblan69@gmail.com>
2025-03-29 01:47:03 +0900

    feat: 딜러, 버퍼 분류

--- src/main/java/com/partymaker/party/PartyService.java
+++ src/main/java/com/partymaker/party/PartyService.java
@@ -1,7 +1,9 @@
 package com.partymaker.party;
 
+import com.partymaker.party.dto.CharacterInfo;
 import com.partymaker.party.dto.GetResponse;
 import com.partymaker.party.dto.response.GetCharacterResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,14 +16,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.time.Duration;
 import java.util.List;
 import java.util.UUID;
 
 
 @Service
+@Slf4j
 public class PartyService {
 
     @Value("${DNF.api-key}")
@@ -48,7 +49,13 @@ public class PartyService {
 
         // HTTP GET 요청 보내기
         GetResponse response = restTemplate.getForObject(apiUrl, GetResponse.class);
-        long power = getPower(response.rows().get(0).characterId(), serverId);
+
+        if(response == null){
+            return null;
+        }
+        CharacterInfo info = response.rows().get(0);
+
+        long power = getPower(info.characterId(), serverId, info.jobGrowName());
 
         return GetCharacterResponse.of(response, power);
     }
@@ -58,12 +65,7 @@ public class PartyService {
         return url;
     }
 
-    private long getPower(String characterId, String serverId) {
-
-        String power = "0";
-
-        // 1. WebDriver와 ChromeDriver 설정
-        // 프로젝트 폴더 기준으로 chromedirver.exe 파일의 위치를 작성
+    private long getPower(String characterId, String serverId, String jobName) {
         String os = System.getProperty("os.name").toLowerCase();
         WebDriver driver;
 
@@ -82,20 +84,37 @@ public class PartyService {
             driver = new ChromeDriver(options);
         }
 
+        String url = "https://dundam.xyz/character?server=" + serverId + "&key=" + characterId;
+        String power = crawl(driver, url, jobName);
+
+        return Long.parseLong(power.replaceAll(",", ""));
+    }
+
+    private String crawl(WebDriver driver, String url, String jobName){
+        String power = "0";
+
         try {
             // 웹 페이지 접속
-            String url = "https://dundam.xyz/character?server=" + serverId + "&key=" + characterId;
             driver.get(url);
 
             // WebDriverWait을 사용해 요소가 나타날 때까지 기다리기
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-            WebElement dvalElement = wait.until(
-                    ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content-container > div.new-cinfo > div.c-aba-stat > div > div.abas-bottom > div.abbot-alldeal > div > div > div > div > div > span.dval"))
-            );
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1));
 
-            // 값 가져오기
-            power = dvalElement.getText();
-            System.out.println("크롤링된 값: " + power);
+            if(isBuffer(jobName)){
+                WebElement dvalElement = wait.until(
+                        ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content-container > div.new-cinfo > div.c-aba-stat > div > div.abas-bottom > div.abbot-alldeal > div > div > div > div > div > span.dval"))
+                );
+                power = dvalElement.getText();
+            }
+
+            else{
+                WebElement dvalElement = wait.until(
+                        ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content-container > div.new-cinfo > div.c-aba-stat > div > div.abas-bottom > div.abbot-alldeal > div > div > div:nth-child(8) > div > div > span.dval"))
+                );
+                power = dvalElement.getText();
+            }
+
+            log.info("크롤링 값: " + power);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,18 +123,10 @@ public class PartyService {
             driver.quit();
         }
 
-        return Long.parseLong(power.replaceAll(",", ""));
+        return power;
     }
 
-    public static String encodeURIComponent(String component) {
-        String result = null;
-
-        try {
-            result = URLEncoder.encode(component, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
-
-        return result;
+    private boolean isBuffer(String jobName){
+        return jobName.matches(".*(인챈|뮤즈|크루).*");
     }
 }
